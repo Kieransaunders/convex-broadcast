40e1755501541daed524f653b55e069b1221a44c
Kieran Saunders <kieran@example.com>
2026-03-12 12:44:18 +0000

    Exclude static assets from SSR function

--- netlify/functions/ssr.js
+++ netlify/functions/ssr.js
@@ -6,4 +6,5 @@ export default async (request, context) => {
 
 export const config = {
   path: '/*',
+  excludedPath: ['/assets/**', '/favicon.ico', '/manifest.json', '/sw.js', '/robots.txt', '/icon-*.png', '/apple-touch-icon.png', '/favicon-*.png', '/android-chrome-*.png'],
 }
